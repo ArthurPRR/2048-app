da26dc1a07ca2243f874245755e9795475967161
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2026-01-28 01:29:56 +0100

    feat: add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,11 @@
-import vueParser from 'vue-eslint-parser'
-import tsParser from '@typescript-eslint/parser'
-import pluginVue from 'eslint-plugin-vue'
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default [
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: tsParser,
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-      }
-    },
-    plugins: {
-      vue: pluginVue
-    },
-    rules: {
-      'vue/multi-word-component-names': 'off'
+export default withNuxt({
+  files: ['**/*.vue'],
+  languageOptions: {
+    parserOptions: {
+      parser: '@typescript-eslint/parser'
     }
   }
-]
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint'
-  ],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
+
   css: ['~/assets/css/main.css'],
 
   app: {
